c2b18b9a962748f088f0de2998926dc55ef70868
Tim <tgorsuch@redventures.com>
2021-02-07 13:04:02 -0500

    centers titlescreen component into background

--- src/js/Components/Background/Background.js
+++ src/js/Components/Background/Background.js
@@ -3,7 +3,7 @@ import TitleScreen from "@js/Components/TitleScreen/TitleScreen";
 
 const Background = () => {
   return (
-    <div className="h-screen  bg-black">
+    <div className="h-screen w-full  bg-black">
       <TitleScreen />
     </div>
   );
--- src/js/Components/TitleScreen/TitleScreen.js
+++ src/js/Components/TitleScreen/TitleScreen.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const TitleScreen = () => {
   return (
-    <div className="h-screen bg-blue flex items-center">
-      <button className="text-white">Info</button>
-      <button className="text-white">Play</button>
+    <div className="h-screen flex justify-center items-center text-center">
+      <div>
+        <h1 className="headline  text-white">TileDrop2.1</h1>
+        <button className="button button--primary  mx-16">Info</button>
+        <button className="button button--secondary  mx-16">Play</button>
+      </div>
     </div>
   );
 };
