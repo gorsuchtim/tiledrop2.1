3d5126df2f57e6702ac47480f56d36b2b2b1e596
Tim <tgorsuch@redventures.com>
2021-02-07 13:16:41 -0500

    adds the gameboard component w/ placeholder content

---
+++ src/js/Components/GameBoard/GameBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GameBoard = () => {
+  return (
+    <div>
+      <h2 className="text-white">Im the game board</h2>
+    </div>
+  );
+};
+
+export default GameBoard;
