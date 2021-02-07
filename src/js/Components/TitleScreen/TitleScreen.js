806a2a2a31bd179f0e027b08a96ea0795f77fbd9
Tim <tgorsuch@redventures.com>
2021-02-07 13:51:47 -0500

    updates todo notes

--- src/js/Components/TitleScreen/TitleScreen.js
+++ src/js/Components/TitleScreen/TitleScreen.js
@@ -1,15 +1,10 @@
-/*
-TODO:
-update a state value to show titlescreenbuttons or gameinfo or gameboard
-default should be titlescreenbuttons and then clicking on one of the buttons should update the state
-to show THAT component
-
-*/
-
 import React from "react";
 import TitleScreenButtons from "@js/Components/TitleScreenButtons/TitleScreenButtons";
 
 const TitleScreen = () => {
+  // state = x.  if state = x show TitleScreenButtons
+  // pass state as prop to that child component from here
+  // for click on buttons to update this state value and show one of the other components on update
   return (
     <div className="bg-black  h-screen flex justify-center items-center text-center">
       <TitleScreenButtons />
