8eb2d3cba4d5a4e3f51ac8a65d25039e38fa69ed
Tim <tgorsuch@redventures.com>
2021-02-07 13:14:29 -0500

    adds the gameinfo component w/ placeholder content

---
+++ src/js/Components/GameInfo/GameInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GameInfo = () => {
+  return (
+    <div>
+      <h2 className="text-white">Im the game info component</h2>
+    </div>
+  );
+};
+
+export default GameInfo;
