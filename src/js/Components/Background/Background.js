cfd3d6e306d3baf4302ab2ec0a43f7e236460d15
Tim <tgorsuch@redventures.com>
2021-02-07 12:55:36 -0500

    creates titlescreen component, imports to bkgrd

--- src/js/Components/Background/Background.js
+++ src/js/Components/Background/Background.js
@@ -1,7 +1,12 @@
 import React from "react";
+import TitleScreen from "@js/Components/TitleScreen/TitleScreen";
 
 const Background = () => {
-  return <div className="h-screen  bg-black"></div>;
+  return (
+    <div className="h-screen  bg-black">
+      <TitleScreen />
+    </div>
+  );
 };
 
 export default Background;
---
+++ src/js/Components/TitleScreen/TitleScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TitleScreen = () => {
+  return (
+    <div className="h-screen bg-blue flex items-center">
+      <button className="text-white">Info</button>
+      <button className="text-white">Play</button>
+    </div>
+  );
+};
+
+export default TitleScreen;
